9217ffed701d6d46f8a95c6ad9beb6714b0a9bd4
Evan Hallmark <ehallmar@usc.edu>
2017-02-04 06:05:39 -0800

    db

--- db/migrate/20170204135136_create_products.rb
+++ db/migrate/20170204135136_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string :upc
-      t.string :title
-      t.float :original_price
-      t.text :description
+      t.string :upc, null: false, uniq: true
+      t.string :title, null: false
+      t.float :original_price, null: false
+      t.text :description, null: false
 
       t.timestamps null: false
     end
