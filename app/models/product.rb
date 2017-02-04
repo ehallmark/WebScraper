b93b59cb7b8bb4fd952907dfb3b5a4b97dd1f6d2
Evan Hallmark <ehallmar@usc.edu>
2017-02-04 06:37:42 -0800

    test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,32 @@
 class Product < ActiveRecord::Base
+
+
+  def self.seedEvents
+    base_url = "http://www.amazon.com/s/field-keywords="
+    base_uri = URI.parse(URI.escape(base_url))
+
+    semaphore = Mutex.new # For multithreading
+    threads = []
+
+    # Set up HTTP Streaming
+    Net::HTTP.start(base_uri.host, base_uri.port) do |http|
+
+      # List all possible nums
+      ["chair"].each do |keyword|
+
+        url = [base_url,"#{keyword}"].join # Generate URL
+        uri = URI.parse(URI.escape(url))
+
+        request = Net::HTTP::Get.new uri
+        results = http.request request
+
+        next unless results.kind_of? Net::HTTPSuccess
+
+        puts "SUCCESSFULLY FOUND: #{keyword}"
+        puts results.body
+
+      end
+    end
+    threads.each do |t| t.join end
+  end
 end
